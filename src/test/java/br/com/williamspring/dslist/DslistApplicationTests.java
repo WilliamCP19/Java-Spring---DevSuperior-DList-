9511362bad2d6a3e18794f0b970624a116f723c9
William da Cruz Pires <williamcruzpires@alunos.utfpr.edu.br>
2023-11-15 17:08:06 -0300

    Criação do Projeto

---
+++ src/test/java/br/com/williamspring/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.williamspring.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
